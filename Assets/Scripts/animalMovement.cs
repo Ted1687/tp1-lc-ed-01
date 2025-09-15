9e38fd97a5ffd7a2d93bff7fbbfc079dddc64f90
StLoadingEternal <2341056@cegeplimoilou.ca>
2025-09-15 09:42:40 -0400

    ajout du prefab animal
    ajout du prefab animal., et creation du script de spawn

---
+++ Assets/Scripts/animalMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class cowMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    public Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
