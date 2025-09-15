228d8f4625787468a938f30144dc74c206dbdc04
Ted1687 <damoieric16@gmail.com>
2025-09-15 09:45:28 -0400

    Ajustement de ground
    - Ajout d'un box Collider au ground

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 10f;
+    public float speed = 10f;
     
-    private float turnSpeed = 5f;
+    public float turnSpeed = 5f;
 
-    public float horizontalInput;
+    private float horizontalInput;
+
+    private float leftBound  = 4;
+
+    private float rightBound = 16;
 
     private Animator playerAnim;
+
     public float verticalInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,13 +26,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
+        //horizontalInput = Input.GetAxis("Horizontal");
 
-        var direction = new Vector3(1, 0, 0);
-        direction = Vector3.right;
-
-        transform.Translate(Vector3.left * speed * verticalInput * Time.deltaTime);
-
-        transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput * turnSpeed);
+       
+        if (! (transform.position.x < leftBound &&  transform.position.x > rightBound) )
+        {
+            transform.Translate(Vector3.left * speed * verticalInput * Time.deltaTime);
+            transform.Rotate(Vector3.left * Time.deltaTime * horizontalInput * turnSpeed);
+        }
+        
     }
 }
